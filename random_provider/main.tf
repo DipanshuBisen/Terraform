e90b72af5d14801dfe5f8b7534dfb89998d010f3
Dipanshu Bisen <dipanshubisen15@gmail.com>
2026-04-14 11:20:29 +0530

    used s3 in random provider

--- random_provider/main.tf
+++ random_provider/main.tf
@@ -1,10 +1,19 @@
 terraform {
   required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.54.1"
+    
+    }
     random = {
       source  = "hashicorp/random"
       version = "3.8.1"
     }
   }
+}
+
+provider "aws" {
+  region = "ap-south-1"
 }
 
 provider "random" {
@@ -15,6 +24,15 @@ resource "random_id" "rand_id" {
   byte_length = 8
 }
 
+resource "aws_s3_bucket" "bucket_tag" {
+  bucket = "dipanshu-${random_id.rand_id.hex}"
+}
+
+resource "aws_s3_object" "bucket_obj" {
+    bucket = "dipanshu-${random_id.rand_id.hex}"
+    source = "./text.txt"
+    key = "text1.txt"
+}
 output "name" {
   value = random_id.rand_id.hex
 }
