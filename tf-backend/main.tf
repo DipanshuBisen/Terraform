03e68ed7cce22341a51beb8d565562c0d0cc0620
Dipanshu Bisen <dipanshubisen15@gmail.com>
2026-04-14 12:37:59 +0530

    work with backend block

---
+++ tf-backend/main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.40.0"
+    }
+  }
+  backend "s3" {
+    bucket = "dipanshu-7ea935fdf4424ea3"
+    key = "backend.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "myserver" {
+    ami = "ami-05d2d839d4f73aafb"
+    instance_type = "t3.micro"
+    tags = {
+      Name = "Sample"
+    }
+}
